62b83b6531338758787299dc5e92e5297508382f
IrenYan <Iren.Yan@hotmail.com>
2019-03-27 19:11:53 +0800

    integrate Task Schedule

--- src/main/java/com/allinpay/DemoApplication.java
+++ src/main/java/com/allinpay/DemoApplication.java
@@ -2,10 +2,12 @@ package com.allinpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.allinpay.repository.mapper")
+@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/allinpay/core/common/ScheduleJobConfig.java
@@ -0,0 +1,25 @@
+package com.allinpay.core.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 定时任务配置
+ */
+@Slf4j
+@Component
+@PropertySource("classpath:resource.properties")
+public class ScheduleJobConfig implements CommandLineRunner {
+    @Scheduled(cron = "${cron.test}")
+    public void print() {
+        System.out.println("我由定时任务调度打印信息~~");
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("我启动后就打印信息~~");
+    }
+}
