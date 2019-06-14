2488e564bb2434245baa1eb1607957cf8a212204
bigBang <462521580@qq.com>
2019-06-14 19:03:38 +0800

    relearn rabbitmq 02

---
+++ src/main/java/com/allinpay/core/util/RabbitUtil.java
@@ -0,0 +1,36 @@
+package com.allinpay.core.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+@Slf4j
+public class RabbitUtil {
+    /** ======================== 定制一些处理策略 =============================*/
+
+    /**
+     * 定制化amqp模版
+     * <p>
+     * ConfirmCallback接口用于实现消息发送到RabbitMQ交换器后接收ack回调   即消息发送到exchange  ack
+     * ReturnCallback接口用于实现消息发送到RabbitMQ 交换器，但无相应队列与交换器绑定时的回调  即消息发送不到任何一个队列中  ack
+     */
+    public static RabbitTemplate rabbitTemplate(RabbitTemplate rabbitTemplate) {
+        // 消息发送失败返回到队列中, yml需要配置 publisher-returns: true
+        rabbitTemplate.setMandatory(true);
+
+        // 消息返回, yml需要配置 publisher-returns: true
+        rabbitTemplate.setReturnCallback((message, replyCode, replyText, exchange, routingKey) -> {
+            String correlationId = message.getMessageProperties().getCorrelationIdString();
+            log.debug("消息：{} 发送失败, 应答码：{} 原因：{} 交换机: {}  路由键: {}", correlationId, replyCode, replyText, exchange, routingKey);
+        });
+
+        // 消息确认, yml需要配置 publisher-confirms: true
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (ack) {
+                log.debug("消息发送到exchange成功,id: {}", correlationData.getId());
+            } else {
+                log.debug("消息发送到exchange失败,原因: {}", cause);
+            }
+        });
+        return rabbitTemplate;
+    }
+}
