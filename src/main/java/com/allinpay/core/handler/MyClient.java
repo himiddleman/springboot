eb7f0779e3860a4622aaaec09a40c8423ce8ca2d
bigBang <462521580@qq.com>
2019-08-12 11:44:54 +0800

    mina client sync accept server response

--- src/main/java/com/allinpay/core/handler/MyClient.java
+++ src/main/java/com/allinpay/core/handler/MyClient.java
@@ -3,7 +3,7 @@ package com.allinpay.core.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.mina.core.RuntimeIoException;
 import org.apache.mina.core.future.ConnectFuture;
-import org.apache.mina.core.future.WriteFuture;
+import org.apache.mina.core.future.ReadFuture;
 import org.apache.mina.core.service.IoConnector;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
@@ -21,7 +21,7 @@ public class MyClient {
     private static int PORT = 9999;
 
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 50; i++) {
             // 创建一个非阻塞的客户端客户端   the non-blocking Socket transport IoConnector
             IoConnector connector = new NioSocketConnector();
             // 设置链接超时时间
@@ -33,8 +33,10 @@ public class MyClient {
                     new ProtocolCodecFilter(new TextLineCodecFactory(Charset
                             .forName("UTF-8"), LineDelimiter.UNIX.getValue(),
                             LineDelimiter.UNIX.getValue())));
-            // 添加业务逻辑处理器类
-            connector.setHandler(new MinaClientHandler());
+            // 添加业务逻辑处理器类 异步处理消息
+//            connector.setHandler(new MinaClientHandler());
+            //同步处理消息 用于客户端，服务端禁用
+            connector.getSessionConfig().setUseReadOperation(true);
             IoSession session;
             for (; ; ) {
                 try {
@@ -48,8 +50,17 @@ public class MyClient {
                     Thread.sleep(5000);
                 }
             }
-            WriteFuture mina = session.write("mina");
-            mina.awaitUninterruptibly();
+            //发送消息
+            session.write("mina" + i).awaitUninterruptibly();
+
+            //同步接受消息
+            ReadFuture read = session.read();
+            if (read.awaitUninterruptibly(2000)) {
+                String str = (String) read.getMessage();
+                System.out.println(str);
+            } else {
+                log.warn("=========消息接受超时=======");
+            }
             log.info("Mina要关闭了:" + i);
             connector.dispose();
         }
