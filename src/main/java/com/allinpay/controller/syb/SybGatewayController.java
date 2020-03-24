b708a4b455380be63e166738b39fc870cc45a72e
bigBang <462521580@qq.com>
2020-03-24 15:50:11 +0800

    update jar version

--- src/main/java/com/allinpay/controller/syb/SybGatewayController.java
+++ src/main/java/com/allinpay/controller/syb/SybGatewayController.java
@@ -3,14 +3,16 @@ package com.allinpay.controller.syb;
 import com.allinpay.core.constant.syb.SybConstant;
 import com.allinpay.core.util.syb.HttpConnectionUtil;
 import com.allinpay.core.util.syb.MD5Util;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 import java.util.TreeMap;
 
-@RestController
+@Controller
 @RequestMapping("/syb/gateway")
 public class SybGatewayController {
     public static void main(String[] args) throws Exception {
@@ -24,7 +26,8 @@ public class SybGatewayController {
 //        params.put("notifyurl", SybConstant.NOTFIY_URL);
         params.put("trxamt", "1");
 //        params.put("gateid", "0105");
-        params.put("orderid", "shlh111038");
+        params.put("orderid", "shlh12345");
+        params.put("gateid", "0103");
         params.put("randomstr", System.currentTimeMillis() + "");
         params.put("paytype", "B2C");
         params.put("sign", MD5Util.sign(params, SybConstant.APPKEY));
@@ -110,21 +113,18 @@ public class SybGatewayController {
         params.put("returl", SybConstant.RET_URL);
 //        params.put("notifyurl", SybConstant.NOTFIY_URL);
         params.put("trxamt", "1");
-        params.put("gateid", "0105");
+//        params.put("gateid", "0105");
         params.put("orderid", "shlh011038");
         params.put("randomstr", System.currentTimeMillis() + "");
         params.put("paytype", "B2C");
         params.put("sign", MD5Util.sign(params, SybConstant.APPKEY));
-        byte[] bys = http.postParams(params, true);
-        String result = new String(bys, "UTF-8");
-        System.out.println("ret:" + result);
-        String content = "<html><head><body><form action=\"http://localhost:8080/jump/redirect\" name=\"postform\" method=\"post\"><input type=\"hidden\" name=\"reqMsg\" value=\"2E81303434383030303030303030303031393939393030303120202000003030303020202020202000303030303030323030B22040800061800A000000001000004130303030303030303030303030303030313030343137313433393534373332373938343831363330393930343430313438313636303030DAE4C4B0BFC6BCBCD3D0CFDEB9ABCBBE20202020202020202020202020202020202020202020202031303141534744303136DAE4C4B0BFC6BCBCD3D0CFDEB9ABCBBE4F443031383131313939343132303030303733323739384F4C3030323139534130323431323139303736373931313031313125CDA8C1AAD6A7B8B6544C303134323031393034313731343439353431353630353530302020202020202020202020202020202020202020202020202020202020204355504E4D7C7C7C3132322E3232342E39302E3232367C30373968747470733A2F2F7673702E616C6C696E7061792E636F6D2F6170697765622F676174657761792F63616C6C6261636B2F6974733F6F7264657249643D313131393934313230303030373332373938303830313035303030303031383131313939343132303030303733323739380000000000000000\"/></form></body><script type=\"text/javascript\">document.forms['postform'].submit();</script></html>";
+        //方式一：直接完成重定向
+//        return "redirect:"+SybConstant.HOSTNAME + "/pay?"+assembleParams(params);
+
+        //方式二：使用response 往前端写html
+        byte[] bytes = http.postParams(params, true);
         response.setContentType("text/html;charset=utf-8");
-        response.getWriter().print(result);
-//        Map<String, String> map = handleResult(result);
-//        for (Map.Entry<String, String> entry : map.entrySet()) {
-//            System.out.println(entry.getKey() + ":" + entry.getValue());
-//        }
+        response.getWriter().print(new String(bytes, "UTF-8"));
     }
 
     private static Map<String, String> handleResult(String result) throws Exception {
@@ -146,5 +146,21 @@ public class SybGatewayController {
         } else {
             throw new Exception(map.get("retmsg").toString());
         }
+    }
+
+    public static String assembleParams(Map<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder outBuf = new StringBuilder();
+        boolean isNotFirst = false;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (isNotFirst)
+                outBuf.append('&');
+            isNotFirst = true;
+            outBuf
+                    .append(entry.getKey())
+                    .append('=')
+                    .append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        System.out.println("参数:" + outBuf.toString());
+        return outBuf.toString();
     }
 }
